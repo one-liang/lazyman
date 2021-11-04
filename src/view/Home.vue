158c65c2fe351d33d2852739b25eecb1329c99ab
william810212 <william810212@gmail.com>
2021-11-05 00:06:15 +0800

    chore: set vuex default config

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,34 @@
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const store = useStore();
+
+    const testState = computed(() => store.state.test);
+    const testGetters = computed(() => store.getters.getTest);
+    const testIncrement = () => store.commit("ADD_TEST", 10);
+    const testActions = () => store.dispatch("asyncTest", 20);
+
+    return {
+      testState,
+      testGetters,
+      testIncrement,
+      testActions,
+    };
+  },
+};
 </script>
 
 <template>
   <h1>Home</h1>
+  <p>store state: {{ testState }}</p>
+  <p>store getters: {{ testGetters }}</p>
+  <div>
+    <button @click="testIncrement" type="button">store mutations +10</button>
+  </div>
+  <div>
+    <button @click="testActions" type="button">store actions +20</button>
+  </div>
 </template>
