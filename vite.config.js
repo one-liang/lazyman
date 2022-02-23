37fc6e0c50e7ca232b78dcd396a7cbe454f4869d
Liang <william810212@gmail.com>
2022-02-23 16:07:29 +0800

    update: related dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
-import eslintPlugin from 'vite-plugin-eslint';
 import Components from 'unplugin-vue-components/vite';
 
 export default defineConfig({
@@ -10,9 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [
-    vue(),
-    Components(),
-    eslintPlugin(),
-  ],
+  plugins: [vue(), Components()],
 });
